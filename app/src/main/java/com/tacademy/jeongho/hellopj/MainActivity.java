f437f79b2b8f4cb49ca7ed68f77b3af4ba1d6398
jeongho <jeongho9496@daum.net>
2016-01-11 11:26:52 +0900

    update01.11

--- app/src/main/java/com/tacademy/jeongho/hellopj/MainActivity.java
+++ app/src/main/java/com/tacademy/jeongho/hellopj/MainActivity.java
@@ -1,17 +1,18 @@
 package com.tacademy.jeongho.hellopj;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textView;
-    private EditText editText;
-    private Button btn;
-
+    EditText editText;
+    TextView textView;
     @Override
 //    onXXX -> callback 메소드
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +20,52 @@ public class MainActivity extends AppCompatActivity {
 //        R.layout.activity_main -> 리소스
         setContentView(R.layout.activity_main);
 
-        textView = (TextView)findViewById(R.id.text_message);
         editText = (EditText)findViewById(R.id.edit_input);
-        btn = (Button)findViewById(R.id.btn_send);
+        textView = (TextView)findViewById(R.id.text_message);
+
+        Button btn = (Button)findViewById(R.id.btn_send);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editText.setText(textView.getText().toString());
             }
         });
+
+        btn = (Button)findViewById(R.id.btn_toast);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.toast_message,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn = (Button)findViewById(R.id.btn_google);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                startActivity(intent);
+            }
+        });
+
+        btn = (Button)findViewById(R.id.btn_dial);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:010-4728-9496"));
+                startActivity(intent);
+            }
+        });
+
+        btn = (Button)findViewById(R.id.btn_activity);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,NextActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
---
+++ app/src/main/java/com/tacademy/jeongho/hellopj/NextActivity.java
@@ -0,0 +1,29 @@
+package com.tacademy.jeongho.hellopj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+        Button btn = (Button)findViewById(R.id.btn_finish);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+        Toast.makeText(this,"종료 nono",Toast.LENGTH_SHORT).show();
+    }
+}
